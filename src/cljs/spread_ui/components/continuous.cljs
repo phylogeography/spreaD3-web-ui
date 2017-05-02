500fc46017839b7c7b9d386590fd3ca1c6863adc
fbielejec <fbielejec@gmail.com>
2017-05-02 23:16:08 +0200

    working on views

--- src/cljs/spread_ui/components/continuous.cljs
+++ src/cljs/spread_ui/components/continuous.cljs
@@ -6,17 +6,37 @@
   (println "click")
   )
 
+;; https://mrmcc3.github.io/post/csv-with-clojurescript/
+
+;; (defn upload-button [file-name]
+;;   [:span.upload-label
+;;    [:label
+;;     [:input.hidden-xs-up 
+;;      {:type "file" :accept ".csv" :on-change #(prn %)}]
+;;     [:i.fa.fa-upload.fa-lg]
+;;     #_(or file-name "click here to upload and render csv...")]
+;;    (when file-name 
+;;      [:i.fa.fa-times {:on-click #(prn %)}])])
+
+;; http://jsfiddle.net/4cwpLvae/
+
 (defn tree-button []
   (fn []
-    [:button {:class "btn-upload"
-              :type "button"
-              :on-click #(handle-tree-upload %)}
-     "Load tree file"]))
+
+    [:label {:for "file-upload" :class "custom-file-upload"}
+     [:i {:class "btn-upload"}] "Custom Upload"
+     ]
+    [:input {:class "none" :id "file-upload" :type "file"}]
+    
+    #_ [:input {:class "btn-upload"
+                :type "file"
+                :on-change #(handle-tree-upload %)}]
+    ))
 
 (defn continuous-analysis-component []
   (fn []    
     [:p "CONTINUOUS"]
-    ;; upload-button
+    #_    [upload-button]
     [tree-button]
     
     ))
